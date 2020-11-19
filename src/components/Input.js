b7bc21344806d40d5bb3fc22f2a84762ff14d966
Collin Didier <collin.didier80@gmail.com>
2020-11-19 11:43:43 -0600

    add input field with button control

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
-  }
+// class Input extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-  render() {
-    return (
-      <div>
-        <label>Input Task: </label>
-        <input type="text" name="task" />
-        <button>Add</button>
-      </div>
-    )
-  }
+//   render() {
+//     return (
+//       <div>
+//         <label>Input Task: </label>
+//         <input type="text" name="task" />
+//         <button>Add</button>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Input
+
+
+import React, {useState} from 'react'
+const Input = (props) => {
+  return (
+    <div className="tasks">
+      <label>Input Task: </label>
+      <input type="text" name="task" value={props.value} onChange={(e) => props.handleChange(e)}/>
+      <button onClick={() => props.addTask(props.value)}>Add</button>
+    </div>
+  )
 }
 
 export default Input
