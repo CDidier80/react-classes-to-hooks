a10f7b825f544eb484e0e76673ef5a4ffa2669b4
Collin Didier <collin.didier80@gmail.com>
2020-11-19 11:44:15 -0600

    complete functionality of to-do list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,64 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import Tasks from './Tasks'
+// import Input from './Input'
+
+// class Todolist extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       tasks: []
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="list">
+//         <Input />
+//         <Tasks tasks={this.state.tasks} />
+//       </div>
+//     )
+//   }
+// }
+
+// export default Todolist
+
+
+import React, { useState } from 'react'
 import Tasks from './Tasks'
 import Input from './Input'
 
-class Todolist extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tasks: []
-    }
+const TodoList = () => {
+  const [tasks, manageTasks] = useState([
+    'Wow Much Do',
+    'Much Do Wow',
+    'Walk the doge',
+    'Pet the doge'
+  ])
+
+  const [value, setValue] = useState("")
+
+  const addTask = (newTask) => {
+    let myNewList = [...tasks, newTask]
+    manageTasks(myNewList)
+    console.log(tasks)
   }
 
-  render() {
-    return (
-      <div className="list">
-        <Input />
-        <Tasks tasks={this.state.tasks} />
-      </div>
-    )
+  const handleChange = (e) => {
+    setValue(e.target.value)
   }
+
+  const delListItem = (listItemIndex) => {
+    let taskArray = [...tasks]
+    taskArray.splice(listItemIndex, 1)
+    manageTasks(taskArray)
+  }
+  
+  return (
+    <div className="list">
+      <Input handleChange={handleChange} addTask={addTask} value={value}/>
+      <Tasks tasks={tasks} delListItem={delListItem}/>
+    </div>
+  )
 }
 
-export default Todolist
+export default TodoList
