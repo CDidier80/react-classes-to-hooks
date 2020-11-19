bd6a9e2ee27c9fc2ef2cf59592df998ab87b612e
Collin Didier <collin.didier80@gmail.com>
2020-11-19 11:44:00 -0600

    refractor to functional component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react'
+// import React, { Component, useState} from 'react'
 
-class Tasks extends Component {
-  constructor(props) {
-    super(props)
-  }
+// class Tasks extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-  render() {
-    return (
-      <ul>
-        {this.props.tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
-    )
-  }
+//   render() {
+//     return (
+//       <ul>
+//         {this.props.tasks.map((task, index) => (
+//           <li key={index}>{task}</li>
+//         ))}
+//       </ul>
+//     )
+//   }
+// }
+
+// export default Tasks
+
+
+import React from 'react'
+
+export default function Tasks(props) {
+  // const [task, setTask] = useState("")
+  return (
+    <ul>
+    {props.tasks.map((task, index) => (
+      <div>
+        <li key={index}>{task}</li>
+        <button key={`button${index}`} onClick={()=> props.delListItem(index)}  >X</button>
+      </div>
+    ))}
+  </ul>
+  )
 }
-
-export default Tasks
